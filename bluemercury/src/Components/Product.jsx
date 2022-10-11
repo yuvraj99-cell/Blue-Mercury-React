44f43341055c20ba8d3edb07ba3463e5650e4e7e
yuvrajcell-99 <yyyuvraj11@gmail.com>
2022-10-11 19:36:09 +0530

    data

---
+++ bluemercury/src/Components/Product.jsx
@@ -0,0 +1,11 @@
+import { Box, Heading, Image, Text, Show, Hide } from '@chakra-ui/react';
+import {  useEffect, useState } from 'react';
+import { Carousel } from '@trendyol-js/react-carousel';
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
+
+
+export const Product=()=> {
+<Box>
+
+</Box>
+}

30b9e91beef0d319466e9f29412c00a435d090e4
vinay <vinaykhairnar9404@gmail.com>
2022-10-15 14:39:18 +0530

    user static complete

--- bluemercury/src/Component/UserAccount.jsx
+++ bluemercury/src/Component/UserAccount.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, Heading, Text } from "@chakra-ui/react";
+import { Box,Button,Flex, Grid, Heading, Text, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 import React from "react";
@@ -31,7 +31,7 @@ const UserAccount = () => {
             </Flex>
           </Flex>
           
-          <Flex marginLeft={200} flexDirection="column">
+          <Flex marginLeft={300} flexDirection="column">
           <Flex alignItems="center">
             <Text fontSize="14px" fontWeight="light" color="gray.600">My Account  </Text>
             <Text fontWeight="500"> {" >"} Account Overview</Text>
@@ -40,6 +40,53 @@ const UserAccount = () => {
           </Flex>
         </Flex>
       </Grid>
+      <Flex justifyContent="space-between"  h="500">
+          <VStack   w="30%" spacing={5}  alignItems="left" >
+          <Box w="90%" px={10} py="6px" border="1px" borderColor="#12284c" bgColor="#12284c" color="white">Account Overview</Box>
+          <Box w="80%" px={10} py="6px" border="1px" bgColor="transparent">My BlueRewards</Box>
+          <Box w="80%" px={10} py="6px" border="1px" bgColor="transparent">My Wishlist</Box>
+          <Box w="80%" px={10} py="6px" border="1px" bgColor="transparent">My Purchases</Box>
+          <Box w="80%" px={10} py="6px" border="1px" bgColor="transparent">Account Details + Preferences</Box>
+          </VStack>
+          <Flex flexDirection="column" w="70%" overflow="scroll" sx={
+     { 
+    '::-webkit-scrollbar':{
+           display:'none'
+       }
+    }
+  }>
+            <Box marginBottom="10">
+              <Text paddingBottom="3" fontWeight="bold">MY BLUEREWARDS</Text>
+              <Box p="15px" w="100%"  border="1px">
+                  <Text p="15px" >Earn $10 for every $250 you spend, plus free birthday gifts & treatments</Text>
+                  <Button p="15px" bgColor="#12284c" color="white" borderRadius="none">JOIN NOW</Button>
+              </Box>
+            </Box>
+
+            <Box marginBottom="10">
+              <Text paddingBottom="3" fontWeight="bold">MY PURCHASES</Text>
+              <Box p="15px" w="100%"  border="1px">
+                  <Text p="15px" >You have not placed any orders.</Text>
+              </Box>
+            </Box>
+
+            <Box marginBottom="10">
+              <Text paddingBottom="3" fontWeight="bold">MY WISHLIST</Text>
+              <Box p="15px" w="100%"  border="1px">
+                  <Text p="15px" >There are no products in your wishlist</Text>
+              </Box>
+            </Box>
+
+            <Box marginBottom="10">
+              <Text paddingBottom="3" fontWeight="bold">MY ACCOUNT INFORMATION</Text>
+              <Box p="15px" w="100%"  border="1px">
+                  <Text p="15px" >There are no profile information</Text>
+              </Box>
+            </Box>
+          
+            
+          </Flex>
+      </Flex>
     </Box>
   );
 };

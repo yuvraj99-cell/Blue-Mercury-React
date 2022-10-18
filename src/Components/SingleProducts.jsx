e7b31b08275a87a8b31c01c4b0dc884978ce71d1
yuvrajcell-99 <yyyuvraj11@gmail.com>
2022-10-18 13:05:32 +0530

    product update

--- src/Components/SingleProducts.jsx
+++ src/Components/SingleProducts.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Text, VStack } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -15,18 +15,32 @@ export const SingleProducts = () => {
   },[id]);
   return (
     <Flex>
-      <Box>
-        <Image src={data.ProductCard__Image_src}></Image>
+      <Flex  color="#F8F8F8"  >
+
+        <Box bgColor="#F8F8F8" border="1px solid black "  ml="25px"  h="100px" w="100px" mr="25px" >
+        <Image  src={data.ProductCard__Image_src}></Image>
+        </Box>
+      <Box  border="1px solid black"    color="#F8F8F8"    h="600px" w="600px" >
+        <Image h="450px" w="450px"    bgColor="white"  p="auto"  mt="60px" ml="60px" src={data.ProductCard__Image_src}></Image>
       </Box>
+      </Flex>
+      <Box   ml="25px" w="650px"   >
+        <VStack alignItems="left" ml="4" mt="10" >
+          <Text fontSize="xl">{data.ProductCard__Brand}</Text>
 
-      <Box>
-        <VStack>
-          <Text>{}</Text>
+          <Text fontSize="3xl"  > {data.ProductCard__Title}</Text>
 
-          <Text></Text>
-
-          <Text></Text>
+          <Text fontSize="sm" fontWeight="bold" >{data.MerchBadge}</Text>
+          <Text   fontSize="md" fontWeight="bold" >${data.ProductCard__Price}</Text>
+          <Text mb="50px"  >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem explicabo quo ab nobis doloremque amet corporis! Similique dolorem ducimus praesentium ab sapiente expedita consectetur commodi. Sint provident explicabo libero adipisci?</Text>
+         
         </VStack>
+
+        <Box mt="50px" bgColor="#F8F8F8"  h="150px" textAlign="center" >
+          <Button mt="10" bgColor="#12274B" w="600px"  mb="-10" color="white" >ADD TO BAG</Button>
+          <Button mt="10" w="600px" bgColor="white" border="1px solid black" borderRadius={0} color="black" >SHIPPING & RETURNS</Button>
+          
+          </Box>
       </Box>
     </Flex>
   );
